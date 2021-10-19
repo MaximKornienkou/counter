6e2d0f61f1300b7084fc972cf84abc23cbe9d33f
MaximKornienkou <82660318+MaximKornienkou@users.noreply.github.com>
2021-10-19 19:54:42 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Settings} from "./components/Settings";
+import {Increment} from "./components/Increment";
+import {storage} from "./storage/storage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const initMaxValue = storage.getItem("maxValue") ? storage.getItem("maxValue") : 10;
+    const initStartValue = storage.getItem("startValue") ? storage.getItem("startValue") : 0;
+
+
+    const [maxValue, setMaxValue] = useState(initMaxValue);
+    const [startValue, setStartValue] = useState(initStartValue);
+    const [incrementValue, setIncrementValue] = useState(startValue);
+    const [disabledSetButton, setDisabledSetButton] = useState(true);
+    const [disabledIncResetButton, setDisabledIncResetButton] = useState(false);
+    const [error, setError] = useState("Incorrect value!")
+
+
+    const increaseValue = () => {
+        setIncrementValue(incrementValue + 1)
+    }
+    const resetValue = () => {
+        setIncrementValue(startValue);
+    }
+
+    const onChangeStartValue = (value: number) => {
+        if (value < 0 || value >= maxValue) {
+            setIncrementValue(error);
+            setStartValue(value);
+            setDisabledSetButton(true);
+        } else {
+            setStartValue(value);
+            setDisabledSetButton(false);
+            setDisabledIncResetButton(true);
+            setIncrementValue("enter values and press 'set'")
+
+        }
+    }
+    const onChangeMaxValue = (value: number) => {
+        if (value <= startValue || startValue < 0) {
+            setIncrementValue(error);
+            setMaxValue(value);
+            setDisabledSetButton(true);
+            setDisabledIncResetButton(true);
+        } else {
+            setMaxValue(value);
+            setDisabledSetButton(false);
+            setDisabledIncResetButton(true);
+            setIncrementValue("enter values and press 'set'");
+        }
+    }
+
+    const startIncrementValue = () => {
+        setIncrementValue(startValue);
+        setDisabledSetButton(true);
+        setDisabledIncResetButton(false);
+        storage.setItem("maxValue", maxValue);
+        storage.setItem("startValue", startValue);
+    }
+
+    return (
+        <div className="container">
+            <Settings onChangeStartValue={onChangeStartValue}
+                      onChangeMaxValue={onChangeMaxValue}
+                      startIncrementValue={startIncrementValue}
+                      disabledSetButton={disabledSetButton}
+                      startValue={startValue}
+                      maxValue={maxValue}
+            />
+            <Increment increaseValue={increaseValue}
+                       incrementValue={incrementValue}
+                       maxValue={maxValue}
+                       resetValue={resetValue}
+                       disabledIncResetButton={disabledIncResetButton}
+            />
+        </div>
+    );
 }
 
 export default App;
